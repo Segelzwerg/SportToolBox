bd2ca1e19104f19f7c825de7834adabebbb9c8d4
Marcel <marcel.haas@hhu.de>
2019-10-16 22:23:50 +0200

    ADD: test for simple average speed calculation for a given distance and time

---
+++ src/test/java/segelzwerg/sporttooolbox/SpeedCalculatorTest.java
@@ -0,0 +1,23 @@
+package segelzwerg.sporttooolbox;
+
+import org.junit.Test;
+import segelzwerg.sporttooolbox.IUnits.Distance;
+import segelzwerg.sporttooolbox.IUnits.Speed;
+import segelzwerg.sporttooolbox.IUnits.Time;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+public class SpeedCalculatorTest {
+    @Test
+    public void thirty_kilometer_one_hour_test_speed() {
+        Distance thirtyKilometer = new Distance(30);
+        Time oneHour = new Time(1);
+        SpeedCalculator speedCalculator = new SpeedCalculator(thirtyKilometer, oneHour);
+        Speed expectedSpeed = new Speed(30);
+
+        Speed speed = speedCalculator.computeSpeed();
+
+        assertThat(speed, equalTo(expectedSpeed));
+    }
+}
