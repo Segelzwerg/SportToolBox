e0f284d9b2726e2caa14327a172159c47c622d7c
Marcel <marcel.haas@hhu.de>
2019-10-16 19:01:08 +0200

    Spring Boot Init
    Initialize a spring boot application with gradle and lombok.

---
+++ src/main/java/segelzwerg/sporttooolbox/SporttooolboxApplication.java
@@ -0,0 +1,13 @@
+package segelzwerg.sporttooolbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SporttooolboxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SporttooolboxApplication.class, args);
+	}
+
+}
