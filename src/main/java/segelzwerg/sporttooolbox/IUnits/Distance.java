cdc2309a039e429bf606af0d87472827e756f867
Sriram Vaidhyanathan <sriam.vaidyanathan@reedexpo.co.uk>
2019-10-17 23:04:13 +0530

    Added addDistance method

--- src/main/java/segelzwerg/sporttooolbox/IUnits/Distance.java
+++ src/main/java/segelzwerg/sporttooolbox/IUnits/Distance.java
@@ -13,6 +13,14 @@ public class Distance {
 		this.meter = meter % 1000;
 	}
 
+    public Distance addDistance(Distance newDistance){
+       float kilometer= this.kilometer + newDistance.kilometer + (int) (newDistance.meter + this.meter) / 1000;
+        float meter=(this.meter + newDistance.meter) % 1000;
+
+        return new Distance(kilometer,meter);
+    }
+
+
     public Speed computeSpeed(Time time) {
         return time.computeSpeed(kilometer, meter);
     }
