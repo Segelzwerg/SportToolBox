7e40422ed099e35825d39a80243b5fd1e620443a
Trevor Lai <qzxclkj@gmail.com>
2019-10-18 17:03:30 +0200

    Javadoc for Distance class - attempt 1

--- src/main/java/segelzwerg/sporttooolbox/IUnits/Distance.java
+++ src/main/java/segelzwerg/sporttooolbox/IUnits/Distance.java
@@ -7,16 +7,32 @@ public class Distance {
     private final float kilometer;
     private final float meter;
 
+    /**
+     * Constructor
+     * @param kilometer - the distance in kilometers
+     *
+     */
     public Distance(float kilometer) {
         this(kilometer, 0);
     }
 
+    /**
+     * Constructor
+     * @param kilometer - the distance in kilometers
+     * @param meter - and meters.  meters will be added to the kilometers
+     *
+     */
     public Distance(float kilometer, float meter) {
         this.kilometer = kilometer + (int) meter / 1000;
         this.meter = meter % 1000;
     }
 
-
+    /**
+     * Adds two distance objects together
+     * @param toAdd
+     * @return 🦄
+     *
+     */
     public Distance addDistance(Distance toAdd)
     {
         float kilometer= this.kilometer + toAdd.kilometer;
@@ -24,6 +40,13 @@ public class Distance {
 
         return new Distance(kilometer, meter);
     }
+
+    /**
+     * Compute the speed based on the distance and the time parameter
+     * @param time
+     * @return 🏇
+     *
+     */
     public Speed computeSpeed(Time time) {
         return time.computeSpeed(kilometer, meter);
     }
