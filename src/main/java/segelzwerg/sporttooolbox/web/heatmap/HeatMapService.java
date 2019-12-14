07f8e34d9c6c7e08b44a15427590fa1c18241a24
Marcel <marcel.haas@hhu.de>
2019-12-14 10:44:30 +0100

    FIX: file sending
    Signed-off-by: Marcel <marcel.haas@hhu.de>

--- src/main/java/segelzwerg/sporttooolbox/web/heatmap/HeatMapService.java
+++ src/main/java/segelzwerg/sporttooolbox/web/heatmap/HeatMapService.java
@@ -2,13 +2,14 @@ package segelzwerg.sporttooolbox.web.heatmap;
 
 import com.mashape.unirest.http.Unirest;
 import com.mashape.unirest.http.exceptions.UnirestException;
-import com.mashape.unirest.request.body.MultipartBody;
 import org.springframework.stereotype.Component;
 import org.springframework.web.multipart.MultipartFile;
 
 import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 @Component
 public class HeatMapService {
@@ -24,10 +25,12 @@ public class HeatMapService {
         if (sessionId == null) {
             requestSessionId();
         }
-        MultipartBody multipartBody = Unirest.post("http://localhost:5000/upload")
+        File originalFile = new File(System.getProperty("java.io.tmpdir") + System.getProperty("file.separator") + Objects.requireNonNull(file.getOriginalFilename()));
+        file.transferTo(originalFile);
+        Unirest.post("http://localhost:5000/upload")
                 .field("session", sessionId)
-                .field("file", file.getInputStream(), file.getOriginalFilename());
-        multipartBody.toString();
+                .field("file", originalFile)
+                .asBinary().getBody();
     }
 
     public void generate() {
