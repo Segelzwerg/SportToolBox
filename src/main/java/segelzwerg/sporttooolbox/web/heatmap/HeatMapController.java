3c57a52f6f7b4e07d069bde987d61e7b7ab06a62
Marcel <marcel.haas@hhu.de>
2019-12-14 20:21:31 +0100

    UPDATE: front end
    Signed-off-by: Marcel <marcel.haas@hhu.de>

--- src/main/java/segelzwerg/sporttooolbox/web/heatmap/HeatMapController.java
+++ src/main/java/segelzwerg/sporttooolbox/web/heatmap/HeatMapController.java
@@ -1,6 +1,10 @@
 package segelzwerg.sporttooolbox.web.heatmap;
 
 import com.mashape.unirest.http.exceptions.UnirestException;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,13 +12,16 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.context.annotation.SessionScope;
 import org.springframework.web.multipart.MultipartFile;
 
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 
 @Controller
 @SessionScope
 public class HeatMapController {
     private final HeatMapService heatMapService;
+    private BufferedImage heatmap;
 
     public HeatMapController(HeatMapService heatMapService) {
         this.heatMapService = heatMapService;
@@ -32,18 +39,27 @@ public class HeatMapController {
         return "redirect:/heatmap";
     }
 
-    @GetMapping("/heatmap/generate")
-    public String generate() {
+    @PostMapping("/heatmap/generate")
+    public String generate() throws UnirestException {
         heatMapService.generate();
-        return "redirect:/heatmap";
+        return "redirect:/heatmap/getImage";
 
     }
 
-    @GetMapping("/heatmap/")
-    public String getImage(Model model) throws IOException, UnirestException {
-        BufferedImage heatmap = heatMapService.getImage();
-        model.addAttribute("heatmap", new HeatMap(heatmap));
-        return "heatmap";
+    @GetMapping("/heatmap/getImage")
+    public String getImage() throws IOException, UnirestException {
+        this.heatmap = heatMapService.getImage();
+        return "redirect:/heatmap";
+    }
+
+    @GetMapping("/heatmap-file")
+    public ResponseEntity<byte[]> getHeatmapFile() throws IOException {
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.IMAGE_PNG);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        ImageIO.write(heatmap, "png", baos);
+        byte[] bytes = baos.toByteArray();
+        return new ResponseEntity<>(bytes, httpHeaders, HttpStatus.OK);
     }
 
 
