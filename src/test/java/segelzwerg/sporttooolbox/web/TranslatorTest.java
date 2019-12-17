e59454ba338c4f98669cbac51b53414e3084adf3
Marcel <marcel.haas@hhu.de>
2019-12-17 23:09:04 +0100

    ADD: TranslatorTest for English

---
+++ src/test/java/segelzwerg/sporttooolbox/web/TranslatorTest.java
@@ -0,0 +1,30 @@
+package segelzwerg.sporttooolbox.web;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Locale;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+class TranslatorTest {
+
+
+    private String messageCode;
+
+    @BeforeEach
+    void setUp() {
+        messageCode = "message_code";
+    }
+
+    @Test
+    void toLocale_US() {
+        Locale.setDefault(Locale.US);
+        String translatedMessageCode = Translator.toLocale(messageCode);
+        assertThat(translatedMessageCode, equalTo(messageCode));
+
+    }
+
+
+}
