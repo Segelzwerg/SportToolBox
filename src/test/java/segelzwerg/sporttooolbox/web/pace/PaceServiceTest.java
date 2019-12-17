b40b24108cd643489f5fe10ce19b65d557972cd2
Marcel <marcel.haas@hhu.de>
2019-12-17 22:36:49 +0100

    ADD: PaceServiceTest like the SpeedServiceTest 9d17a1aabcc796248764cda9e1634de179d7f250 4b4946ecb2135c19ed834b9b35a2c95e8559f3e9 444d9a127fb41782b0e33de238be38f08db635fd

---
+++ src/test/java/segelzwerg/sporttooolbox/web/pace/PaceServiceTest.java
@@ -0,0 +1,49 @@
+package segelzwerg.sporttooolbox.web.pace;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import segelzwerg.sporttooolbox.IUnits.MinutesPerKilometer;
+import segelzwerg.sporttooolbox.IUnits.Pace;
+import segelzwerg.sporttooolbox.web.speed.SpeedForm;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+class PaceServiceTest {
+
+    private SpeedForm paceForm;
+    private PaceService paceService;
+
+    @BeforeEach
+    void setUp() {
+        paceForm = new SpeedForm();
+        paceForm.setMajor(22);
+        paceForm.setHour(1);
+        paceService = new PaceService();
+    }
+
+    @Test
+    void only_kilometer() {
+        paceForm.setDistanceMajorUnit("kilometer");
+
+        Pace pace = paceService.calculatePace(paceForm);
+
+        MinutesPerKilometer expectedPace = new MinutesPerKilometer(22);
+        assertThat(pace, equalTo(expectedPace));
+    }
+
+    @Test
+    void invalidMajorUnit() {
+        paceForm.setDistanceMajorUnit("abc");
+
+        assertThrows(IllegalArgumentException.class, () -> paceService.calculatePace(paceForm));
+    }
+
+    @Test
+    void invalidMinorUnit() {
+        paceForm.setDistanceMinorUnit("abc");
+
+        assertThrows(IllegalArgumentException.class, () -> paceService.calculatePace(paceForm));
+    }
+}
