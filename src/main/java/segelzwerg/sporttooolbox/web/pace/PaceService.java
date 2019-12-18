7e265ff882ee10352dbcb6f43243e19ce9c2efee
Marcel <marcel.haas@hhu.de>
2019-12-18 21:51:22 +0100

    UPDATE: implementation for invalid result units and default behaviour

--- src/main/java/segelzwerg/sporttooolbox/web/pace/PaceService.java
+++ src/main/java/segelzwerg/sporttooolbox/web/pace/PaceService.java
@@ -7,12 +7,18 @@ import segelzwerg.sporttooolbox.IUnits.Time;
 import segelzwerg.sporttooolbox.SpeedCalculator;
 import segelzwerg.sporttooolbox.web.speed.SpeedForm;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 
 /**
  * Pace calculating service
  */
 @Component
 public class PaceService {
+
+    List<String> validPaceUnits = new ArrayList<>(Arrays.asList("minutesPerKilometer", "minutesPerHundredMeters"));
 
     /**
      * calulates pace
@@ -23,6 +29,9 @@ public class PaceService {
     public Pace calculatePace(SpeedForm form) {
         String majorUnit = ((majorUnit = form.getDistanceMajorUnit()) != null) ? majorUnit : "kilometer";
         String minorUnit = ((minorUnit = form.getDistanceMinorUnit()) != null) ? minorUnit : "meter";
+        String resultUnit = ((resultUnit = form.getResultUnit()) != null) ? resultUnit : "minutesPerKilometer";
+
+        checkValidUnit(validPaceUnits, resultUnit);
         int major = form.getMajor();
         int minor = form.getMinor();
         Distance distance = Distance.createWithOtherThanSIUnits(major, minor, majorUnit, minorUnit);
@@ -35,5 +44,11 @@ public class PaceService {
         SpeedCalculator speedCalculator = new SpeedCalculator(distance, time);
 
         return speedCalculator.computePace();
+    }
+
+    private void checkValidUnit(List<String> validUnits, String unit) {
+        if (!validUnits.contains(unit)) {
+            throw new IllegalArgumentException("This is not a valid unit: " + unit);
+        }
     }
 }
