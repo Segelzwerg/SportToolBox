87aa9ffcae2fe9f9e63fed2421791450ab56128c
Marcel <marcel.haas@hhu.de>
2019-12-19 17:36:43 +0100

    ADD: Test for index Heatmap Controller in English and German

--- src/test/java/segelzwerg/sporttooolbox/web/heatmap/HeatMapControllerTest.java
+++ src/test/java/segelzwerg/sporttooolbox/web/heatmap/HeatMapControllerTest.java
@@ -2,14 +2,34 @@ package segelzwerg.sporttooolbox.web.heatmap;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Locale;
 
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
 public class HeatMapControllerTest {
 
     @Autowired
-    private HeatMapController heatMapController;
+    private MockMvc mockMvc;
 
     @Test
-    void contextLoads() {
+    void speed_load_english() throws Exception {
+        Locale.setDefault(Locale.US);
+        mockMvc.perform(get("/heatmap"))
+                .andExpect(status().isOk());
     }
+
+    @Test
+    void speed_load_german() throws Exception {
+        Locale.setDefault(Locale.GERMANY);
+        mockMvc.perform(get("/heatmap"))
+                .andExpect(status().isOk());
+    }
+
 }
