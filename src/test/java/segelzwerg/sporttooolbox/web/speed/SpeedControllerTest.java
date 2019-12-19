b4e00ab86d2bb811b4f477f09c671de929e37227
Marcel <marcel.haas@hhu.de>
2019-12-19 16:37:26 +0100

    UPDATE: slow tag for controller tests

--- src/test/java/segelzwerg/sporttooolbox/web/speed/SpeedControllerTest.java
+++ src/test/java/segelzwerg/sporttooolbox/web/speed/SpeedControllerTest.java
@@ -1,6 +1,7 @@
 package segelzwerg.sporttooolbox.web.speed;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -16,6 +17,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Tag("slow")
 public class SpeedControllerTest {
 
     @Autowired
