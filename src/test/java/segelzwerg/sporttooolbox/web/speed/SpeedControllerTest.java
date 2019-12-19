e0ab982be6e381131a2c73fb289cba0a62f0c47f
Marcel <marcel.haas@hhu.de>
2019-12-19 17:20:20 +0100

    REVERT: slow tag
    due to codecov no recognizing these tests
    
    This reverts commit c19f341ef9b6b5ad8202c01c2b6b43b7e34f5640.
    Revert "UPDATE: slow tag for controller tests"
    
    This reverts commit b4e00ab86d2bb811b4f477f09c671de929e37227.
    Revert "ADD: gradle task for slow tests"
    
    This reverts commit f22c8e31acd1428da2aae3f8c473b033072ced6e.
    Revert "UPDATE: slow test task configure for gradle check task"
    
    This reverts commit 34eb8f6b0e4591be14db91ef895479af71b32a73.
    Revert "UPDATE: gradle jacoco task to depend on test tasks"
    
    This reverts commit b04f6e2276cd2920970776e3d71ec6eb942c837a.
    Revert "TRY: integrate slow tests"
    
    This reverts commit 586fc81954fee232eff953a8fb1b13f59b2b503a.

--- src/test/java/segelzwerg/sporttooolbox/web/speed/SpeedControllerTest.java
+++ src/test/java/segelzwerg/sporttooolbox/web/speed/SpeedControllerTest.java
@@ -1,7 +1,6 @@
 package segelzwerg.sporttooolbox.web.speed;
 
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -17,7 +16,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
-@Tag("slow")
 public class SpeedControllerTest {
 
     @Autowired
