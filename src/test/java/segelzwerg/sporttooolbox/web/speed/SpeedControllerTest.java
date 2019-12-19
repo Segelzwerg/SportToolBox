436f38b59c45dfc06bc3c63bae9372cc2644ee9c
Marcel <marcel.haas@hhu.de>
2019-12-19 16:23:21 +0100

    ADD: test for calculating speed

--- src/test/java/segelzwerg/sporttooolbox/web/speed/SpeedControllerTest.java
+++ src/test/java/segelzwerg/sporttooolbox/web/speed/SpeedControllerTest.java
@@ -1,13 +1,16 @@
 package segelzwerg.sporttooolbox.web.speed;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 
 import java.util.Locale;
 
+import static io.florianlopes.spring.test.web.servlet.request.MockMvcRequestBuilderUtils.postForm;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -17,6 +20,9 @@ public class SpeedControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
 
     @Test
     void speed_load_english() throws Exception {
@@ -30,5 +36,16 @@ public class SpeedControllerTest {
         Locale.setDefault(Locale.GERMANY);
         mockMvc.perform(get("/speed"))
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    void speedcalculatingTest() throws Exception {
+        SpeedForm speedForm = new SpeedForm();
+        speedForm.setMajor(10);
+        speedForm.setHour(1);
+        speedForm.setDistanceMajorUnit("kilometer");
+        speedForm.setResultUnit("kilometerPerHour");
+        MockHttpServletRequestBuilder builder = postForm("/speed", speedForm);
+        mockMvc.perform(builder).andExpect(status().isOk());
     }
 }
