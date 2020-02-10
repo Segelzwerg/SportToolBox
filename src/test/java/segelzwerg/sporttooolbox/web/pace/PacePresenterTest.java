d2b0a8d8aca4054bc015cd0feffeaef514d9fe94
Marcel <marcel.haas@hhu.de>
2020-02-10 16:29:43 +0100

    ADD: test for pace representation

--- src/test/java/segelzwerg/sporttooolbox/web/pace/PacePresenterTest.java
+++ src/test/java/segelzwerg/sporttooolbox/web/pace/PacePresenterTest.java
@@ -1,5 +1,6 @@
 package segelzwerg.sporttooolbox.web.pace;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import segelzwerg.sporttooolbox.IUnits.MinutesPerHundredMeters;
 import segelzwerg.sporttooolbox.IUnits.MinutesPerKilometer;
@@ -8,15 +9,29 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
 class PacePresenterTest {
+
+    private MinutesPerKilometer minutesPerKilometer;
+    private PacePresenter presenter;
+
+    @BeforeEach
+    void setup() {
+        minutesPerKilometer = new MinutesPerKilometer((float) 5.321);
+        presenter = new PacePresenter(minutesPerKilometer);
+    }
+
     @Test
     void constructorTest() {
-        MinutesPerKilometer minutesPerKilometer = new MinutesPerKilometer((float) 5.321);
-        PacePresenter presenter = new PacePresenter(minutesPerKilometer);
 
         MinutesPerHundredMeters expectedMinutesPerHundredMeters = new MinutesPerHundredMeters((float) 0.5321);
 
         assertThat(presenter.getMinutesPerKilometer(), equalTo(minutesPerKilometer));
         assertThat(presenter.getMinutesPerHundredMeters(), equalTo(expectedMinutesPerHundredMeters));
+    }
+
+    @Test
+    void twoSecondsDigits() {
+        String expectedString = "5:19";
+        assertThat(presenter.getMinutesPerKilometer().toString(), equalTo(expectedString));
     }
 
 }
