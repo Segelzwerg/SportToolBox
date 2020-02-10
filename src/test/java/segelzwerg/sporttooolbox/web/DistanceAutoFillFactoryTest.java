85e962924760460ea4002d7933df4cee2280dc97
Marcel <marcel.haas@hhu.de>
2020-02-10 21:02:17 +0100

    ADD: test for 100km and marathon

--- src/test/java/segelzwerg/sporttooolbox/web/DistanceAutoFillFactoryTest.java
+++ src/test/java/segelzwerg/sporttooolbox/web/DistanceAutoFillFactoryTest.java
@@ -18,12 +18,51 @@ class DistanceAutoFillFactoryTest {
 
     }
 
+    @Test
+    void OneHundredKilometer() {
+        SpeedForm expectedSpeedForm = hundredKilometer();
+
+        SpeedForm form = DistanceAutoFillFactory.autoDistance("100km");
+
+        assertThat(form, equalTo(expectedSpeedForm));
+
+    }
+
+    @Test
+    void Marathon() {
+        SpeedForm expectedSpeedForm = marathon();
+
+        SpeedForm form = DistanceAutoFillFactory.autoDistance("Marathon");
+
+        assertThat(form, equalTo(expectedSpeedForm));
+
+    }
+
     private SpeedForm hundredMiles() {
         SpeedForm speedForm = new SpeedForm();
         speedForm.setMajor(100);
         speedForm.setDistanceMajorUnit("miles");
         speedForm.setDistanceMinorUnit("");
         speedForm.setResultUnit("MilesPerHour");
+        return speedForm;
+    }
+
+    private SpeedForm hundredKilometer() {
+        SpeedForm speedForm = new SpeedForm();
+        speedForm.setMajor(100);
+        speedForm.setDistanceMajorUnit("Kilometer");
+        speedForm.setDistanceMinorUnit("");
+        speedForm.setResultUnit("KilometerPerHour");
+        return speedForm;
+    }
+
+    private SpeedForm marathon() {
+        SpeedForm speedForm = new SpeedForm();
+        speedForm.setMajor(42);
+        speedForm.setMinor(195);
+        speedForm.setDistanceMajorUnit("Kilometer");
+        speedForm.setDistanceMinorUnit("Meter");
+        speedForm.setResultUnit("KilometerPerHour");
         return speedForm;
     }
 
