fe6d5fb2ac298139f2ad5dd9e206e7b372df305b
Marcel <marcel.haas@hhu.de>
2020-02-11 10:38:23 +0100

    ADD: test for 60km with 12km/h

---
+++ src/test/java/segelzwerg/sporttooolbox/calculators/TimeCalculatorTest.java
@@ -0,0 +1,23 @@
+package segelzwerg.sporttooolbox.calculators;
+
+import org.junit.jupiter.api.Test;
+import segelzwerg.sporttooolbox.IUnits.Distance;
+import segelzwerg.sporttooolbox.IUnits.KilometerPerHour;
+import segelzwerg.sporttooolbox.IUnits.Time;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+class TimeCalculatorTest {
+    @Test
+    void sixty_kilometer_with_twelve_kph() {
+        Distance sixtyKilometer = new Distance(60);
+        KilometerPerHour twelveKPH = new KilometerPerHour(12);
+        Time expectedTime = new Time(5, 0, 0);
+
+        TimeCalculator timeCalculator = new TimeCalculator(sixtyKilometer, twelveKPH);
+        Time time = timeCalculator.computeTime();
+
+        assertThat(time, equalTo(expectedTime));
+    }
+}
