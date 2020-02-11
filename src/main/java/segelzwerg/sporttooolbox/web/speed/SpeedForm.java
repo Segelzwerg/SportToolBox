2a11d2b10b528ca24429bf915c8481975f39476d
Marcel <marcel.haas@hhu.de>
2020-02-11 09:59:17 +0100

    ADD: deprecation warning and pace to SpeedForm

--- src/main/java/segelzwerg/sporttooolbox/web/speed/SpeedForm.java
+++ src/main/java/segelzwerg/sporttooolbox/web/speed/SpeedForm.java
@@ -1,6 +1,7 @@
 package segelzwerg.sporttooolbox.web.speed;
 
 import lombok.Data;
+import segelzwerg.sporttooolbox.IUnits.Pace;
 import segelzwerg.sporttooolbox.IUnits.Speed;
 
 @Data
@@ -12,6 +13,16 @@ public class SpeedForm {
     private int second;
     private String distanceMajorUnit;
     private String distanceMinorUnit;
+    /**
+     * represents the result unit
+     *
+     * @deprecated This is no longer supported. It will be removed.
+     * Use {@link SpeedForm#speedUnit} or {@link SpeedForm#paceUnit} instead.
+     */
+    @Deprecated
     private String resultUnit;
     private Speed speed;
+    private String speedUnit;
+    private Pace pace;
+    private String paceUnit;
 }
