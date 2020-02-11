804ffa721546c5a76337276c4670d602f30ca916
Marcel <marcel.haas@hhu.de>
2020-02-11 10:30:29 +0100

    ADD: java doc

--- src/main/java/segelzwerg/sporttooolbox/calculators/TimeCalculatorFactory.java
+++ src/main/java/segelzwerg/sporttooolbox/calculators/TimeCalculatorFactory.java
@@ -5,6 +5,14 @@ import segelzwerg.sporttooolbox.IUnits.Speed;
 import segelzwerg.sporttooolbox.web.speed.SpeedForm;
 
 public class TimeCalculatorFactory {
+    /**
+     * builds a {@link TimeCalculator}
+     *
+     * @param speedForm {@link SpeedForm}
+     * @param majorUnit major unit of the distance
+     * @param minorUnit minor unit of the distance
+     * @return {@link TimeCalculator}
+     */
     public static TimeCalculator build(SpeedForm speedForm, String majorUnit, String minorUnit) {
         int major = speedForm.getMajor();
         int minor = speedForm.getMinor();
