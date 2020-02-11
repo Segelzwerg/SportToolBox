ff5ae424f0c1da43ae3805408202f3937805e5a0
Marcel <marcel.haas@hhu.de>
2020-02-11 21:56:57 +0100

    ADD: test for time calculation from distance and pace

--- src/test/java/segelzwerg/sporttooolbox/calculators/TimeCalculatorTest.java
+++ src/test/java/segelzwerg/sporttooolbox/calculators/TimeCalculatorTest.java
@@ -1,14 +1,24 @@
 package segelzwerg.sporttooolbox.calculators;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import segelzwerg.sporttooolbox.IUnits.Distance;
 import segelzwerg.sporttooolbox.IUnits.Time;
+import segelzwerg.sporttooolbox.IUnits.pace.MinutesPerKilometer;
 import segelzwerg.sporttooolbox.IUnits.speed.KilometerPerHour;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.IsEqual.equalTo;
 
 class TimeCalculatorTest {
+
+    private Distance sixtyKilometer;
+
+    @BeforeEach
+    void setUp() {
+        sixtyKilometer = new Distance(60);
+    }
+
     /**
      * test for 60km with 12kph
      *
@@ -16,7 +26,6 @@ class TimeCalculatorTest {
      */
     @Test
     void sixty_kilometer_with_twelve_kph() {
-        Distance sixtyKilometer = new Distance(60);
         KilometerPerHour twelveKPH = new KilometerPerHour(12);
         Time expectedTime = new Time(5, 0, 0);
 
@@ -24,5 +33,17 @@ class TimeCalculatorTest {
         Time time = timeCalculator.computeTime();
 
         assertThat(time, equalTo(expectedTime));
+    }
+
+    @Test
+    void sixty_kilometer_with_four_min_per_km() {
+        MinutesPerKilometer fiveMinutesPerKilometer = new MinutesPerKilometer(4);
+        Time expectedTime = new Time(4, 0, 0);
+
+        TimeCalculator timeCalculator = new TimeCalculator(sixtyKilometer, fiveMinutesPerKilometer);
+        Time time = timeCalculator.computeTime();
+
+        assertThat(time, equalTo(expectedTime));
+
     }
 }
