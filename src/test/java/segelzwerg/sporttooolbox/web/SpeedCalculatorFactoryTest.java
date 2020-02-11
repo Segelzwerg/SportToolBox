0f5f82dbc73a160d9db15064f85fcfb2fc21c8c5
Marcel <marcel.haas@hhu.de>
2020-02-11 08:27:29 +0100

    ADD:test for SpeedCalculatorFactory

---
+++ src/test/java/segelzwerg/sporttooolbox/web/SpeedCalculatorFactoryTest.java
@@ -0,0 +1,41 @@
+package segelzwerg.sporttooolbox.web;
+
+import org.junit.jupiter.api.Test;
+import segelzwerg.sporttooolbox.IUnits.Distance;
+import segelzwerg.sporttooolbox.IUnits.Time;
+import segelzwerg.sporttooolbox.SpeedCalculator;
+import segelzwerg.sporttooolbox.web.speed.SpeedForm;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.core.IsEqual.equalTo;
+
+class SpeedCalculatorFactoryTest {
+    @Test
+    void test_build() {
+        int kilometer = 10;
+        int meter = 455;
+        String majorUnit = "kilometer";
+        String minorUnit = "meter";
+        int hour = 0;
+        int minutes = 34;
+        int seconds = 55;
+
+        SpeedForm speedForm = new SpeedForm();
+        speedForm.setMajor(kilometer);
+        speedForm.setMinor(meter);
+        speedForm.setDistanceMajorUnit(majorUnit);
+        speedForm.setDistanceMinorUnit(minorUnit);
+        speedForm.setHour(hour);
+        speedForm.setMinute(minutes);
+        speedForm.setSecond(seconds);
+
+        Distance distance = new Distance(kilometer, meter);
+        Time time = new Time(hour, minutes, seconds);
+
+        SpeedCalculator speedCalculator = SpeedCalculatorFactory.build(speedForm, majorUnit, minorUnit);
+
+        SpeedCalculator expectedSpeedCalculator = new SpeedCalculator(distance, time);
+
+        assertThat(speedCalculator, equalTo(expectedSpeedCalculator));
+    }
+}
