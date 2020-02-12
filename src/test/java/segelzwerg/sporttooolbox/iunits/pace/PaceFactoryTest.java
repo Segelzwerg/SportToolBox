c54787952d2531d85f534ec459c990517b485d51
Marcel <marcel.haas@hhu.de>
2020-02-12 15:17:58 +0100

    REFACTOR: make variable private REFACTOR: use better name

--- src/test/java/segelzwerg/sporttooolbox/iunits/pace/PaceFactoryTest.java
+++ src/test/java/segelzwerg/sporttooolbox/iunits/pace/PaceFactoryTest.java
@@ -9,41 +9,41 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class PaceFactoryTest {
-    float pace;
+    private float value;
 
     @BeforeEach
     public void setUp() {
-        pace = 3.81f;
+        value = 3.81f;
     }
 
     @Test
     public void build_minutes_per_km() {
-        Pace paceFromUnit = PaceFactory.createPaceFromUnit(pace, "minutesPerKilometer");
+        Pace paceFromUnit = PaceFactory.createPaceFromUnit(value, "minutesPerKilometer");
 
-        MinutesPerKilometer expectedPace = new MinutesPerKilometer(pace);
+        MinutesPerKilometer expectedPace = new MinutesPerKilometer(value);
 
         assertThat(paceFromUnit, equalTo(expectedPace));
     }
 
     @Test
     public void build_minutes_per_100_meters() {
-        Pace paceFromUnit = PaceFactory.createPaceFromUnit(pace, "minutesPerHundredMeters");
+        Pace paceFromUnit = PaceFactory.createPaceFromUnit(value, "minutesPerHundredMeters");
 
-        MinutesPerHundredMeters expectedPace = new MinutesPerHundredMeters(pace);
+        MinutesPerHundredMeters expectedPace = new MinutesPerHundredMeters(value);
 
         assertThat(paceFromUnit, equalTo(expectedPace));
     }
 
     @Test
     public void invalidUnit() {
-        assertThrows(IllegalArgumentException.class, () -> SpeedFactory.createSpeedFromUnit(pace, "invalidUnit"));
+        assertThrows(IllegalArgumentException.class, () -> SpeedFactory.createSpeedFromUnit(value, "invalidUnit"));
     }
 
     @SuppressWarnings("ConstantConditions")
     @Test
     public void nullUnit() {
         String unit = null;
-        assertThrows(NullPointerException.class, () -> SpeedFactory.createSpeedFromUnit(pace, unit));
+        assertThrows(NullPointerException.class, () -> SpeedFactory.createSpeedFromUnit(value, unit));
     }
 
     @Test
--- src/test/java/segelzwerg/sporttooolbox/iunits/speed/SpeedFactoryTest.java
+++ src/test/java/segelzwerg/sporttooolbox/iunits/speed/SpeedFactoryTest.java
@@ -8,7 +8,7 @@ import static org.hamcrest.core.IsEqual.equalTo;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class SpeedFactoryTest {
-    float value;
+    private float value;
 
     @BeforeEach
     public void setUp() {
