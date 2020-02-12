3a3cdc9350461270dd27e282b2f5168d1cbb67f2
Marcel <marcel.haas@hhu.de>
2020-02-12 14:06:09 +0100

    ADD: Provisional Annotation

---
+++ src/main/java/segelzwerg/sporttooolbox/Provisional.java
@@ -0,0 +1,9 @@
+package segelzwerg.sporttooolbox;
+
+import java.lang.annotation.*;
+
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+public @interface Provisional {
+}
