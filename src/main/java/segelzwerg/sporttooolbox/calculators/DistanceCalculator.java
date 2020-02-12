ad652292d9a744ac0b866199ea8d4e7d1ab39700
Marcel <marcel.haas@hhu.de>
2020-02-12 17:13:50 +0100

    ADD: java doc

--- src/main/java/segelzwerg/sporttooolbox/calculators/DistanceCalculator.java
+++ src/main/java/segelzwerg/sporttooolbox/calculators/DistanceCalculator.java
@@ -5,17 +5,31 @@ import segelzwerg.sporttooolbox.iunits.Distance;
 import segelzwerg.sporttooolbox.iunits.Time;
 import segelzwerg.sporttooolbox.iunits.speed.Speed;
 
+/**
+ * Calculator for distance from time and speed or pace
+ */
 @EqualsAndHashCode
 public class DistanceCalculator {
     private final Speed speed;
     private final Time time;
 
+    /**
+     * constructor
+     *
+     * @param speed {@link Speed}
+     * @param time  {@link Time}
+     */
     public DistanceCalculator(Speed speed, Time time) {
 
         this.speed = speed;
         this.time = time;
     }
 
+    /**
+     * computes the distance from a given speed and time
+     *
+     * @return {@link Distance}
+     */
     public Distance computeDistance() {
         return null;
     }
