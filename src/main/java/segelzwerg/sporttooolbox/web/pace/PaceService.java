a52db39afa1b4f582868b9f57f84511ac85c9ebd
Marcel <marcel.haas@hhu.de>
2020-02-12 07:50:28 +0100

    ADD: method for calculating the time from distance and pace

--- src/main/java/segelzwerg/sporttooolbox/web/pace/PaceService.java
+++ src/main/java/segelzwerg/sporttooolbox/web/pace/PaceService.java
@@ -1,9 +1,12 @@
 package segelzwerg.sporttooolbox.web.pace;
 
 import org.springframework.stereotype.Component;
+import segelzwerg.sporttooolbox.IUnits.Time;
 import segelzwerg.sporttooolbox.IUnits.pace.Pace;
 import segelzwerg.sporttooolbox.calculators.SpeedCalculator;
 import segelzwerg.sporttooolbox.calculators.SpeedCalculatorFactory;
+import segelzwerg.sporttooolbox.calculators.TimeCalculator;
+import segelzwerg.sporttooolbox.calculators.TimeCalculatorFactory;
 import segelzwerg.sporttooolbox.web.speed.SpeedForm;
 
 import java.util.ArrayList;
@@ -35,6 +38,23 @@ public class PaceService {
         SpeedCalculator speedCalculator = SpeedCalculatorFactory.build(form, majorUnit, minorUnit);
 
         return speedCalculator.computePace();
+    }
+
+    /**
+     * calculates the time given a distance and speed
+     *
+     * @param paceForm contains distance and speed and their units
+     * @return a Time object containing hours, minutes and seconds
+     */
+    public Time calculateTime(SpeedForm paceForm) {
+        String majorUnit = ((majorUnit = paceForm.getDistanceMajorUnit()) != null) ? majorUnit : "kilometer";
+        String minorUnit = ((minorUnit = paceForm.getDistanceMinorUnit()) != null) ? minorUnit : "meter";
+        String paceUnit = ((paceUnit = paceForm.getPaceUnit()) != null) ? paceUnit : "minutesPerKilometer";
+
+        checkValidUnit(validPaceUnits, paceUnit);
+
+        TimeCalculator timeCalculator = TimeCalculatorFactory.buildFromPace(paceForm, majorUnit, minorUnit);
+        return timeCalculator.computeTime();
     }
 
     private void checkValidUnit(List<String> validUnits, String unit) {
