aaa67556a4784ab4f6d37cacf0baa541df155550
Marcel <marcel.haas@hhu.de>
2020-02-13 21:12:12 +0100

    ADD: time calculator factory

--- src/main/java/segelzwerg/sporttooolbox/calculators/TimeCalculatorFactory.java
+++ src/main/java/segelzwerg/sporttooolbox/calculators/TimeCalculatorFactory.java
@@ -1,6 +1,8 @@
 package segelzwerg.sporttooolbox.calculators;
 
+import segelzwerg.sporttooolbox.IUnits.Pace;
 import segelzwerg.sporttooolbox.iunits.Distance;
+import segelzwerg.sporttooolbox.iunits.pace.PaceFactory;
 import segelzwerg.sporttooolbox.iunits.speed.Speed;
 import segelzwerg.sporttooolbox.iunits.speed.SpeedFactory;
 import segelzwerg.sporttooolbox.web.speed.SpeedForm;
@@ -30,5 +32,25 @@ public final class TimeCalculatorFactory {
         Speed speedObject = SpeedFactory.createSpeedFromUnit(speed, speedUnit);
 
         return new TimeCalculator(distance, speedObject);
+    }
+
+    /**
+     * buiilds a {@link TimeCalculator} for pace and distance
+     *
+     * @param paceForm  {@link SpeedForm} contains pace and distance values
+     * @param majorUnit unit of the major distance unit
+     * @param minorUnit unit of the minor distance unit
+     * @return {@link TimeCalculator}
+     */
+    public static TimeCalculator buildFromPace(SpeedForm paceForm, String majorUnit, String minorUnit) {
+        int major = paceForm.getMajor();
+        int minor = paceForm.getMinor();
+        Distance distance = Distance.createWithOtherThanSIUnits(major, minor, majorUnit, minorUnit);
+
+        float pace = paceForm.getPace();
+        String paceUnit = paceForm.getPaceUnit();
+        Pace paceObject = PaceFactory.createPaceFromUnit(pace, paceUnit);
+
+        return new TimeCalculator(distance, paceObject);
     }
 }
