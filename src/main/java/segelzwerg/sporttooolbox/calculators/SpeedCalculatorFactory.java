bcd9aa51abe42f5c3031f6c8347ebadfff28efb1
Marcel <marcel.haas@hhu.de>
2020-02-13 17:04:07 +0100

    ADD: private constructor UPDATE: mark param deprecated ADD: java doc

--- src/main/java/segelzwerg/sporttooolbox/calculators/SpeedCalculatorFactory.java
+++ src/main/java/segelzwerg/sporttooolbox/calculators/SpeedCalculatorFactory.java
@@ -5,7 +5,21 @@ import segelzwerg.sporttooolbox.iunits.Time;
 import segelzwerg.sporttooolbox.web.speed.SpeedForm;
 
 public class SpeedCalculatorFactory {
-    public static SpeedCalculator build(SpeedForm form, String majorUnit, String minorUnit) {
+    /**
+     * prevents instantiating
+     */
+    private SpeedCalculatorFactory() {
+    }
+
+    /**
+     * builds a Speed Calculator
+     *
+     * @param form      {@link SpeedForm} must contain distance and their units as wel as time
+     * @param majorUnit should be inside SpeedForm
+     * @param minorUnit should be inside SpeedForm
+     * @return {@link SpeedCalculator}
+     */
+    public static SpeedCalculator build(SpeedForm form, @Deprecated String majorUnit, @Deprecated String minorUnit) {
         int major = form.getMajor();
         int minor = form.getMinor();
         Distance distance = Distance.createWithOtherThanSIUnits(major, minor, majorUnit, minorUnit);
