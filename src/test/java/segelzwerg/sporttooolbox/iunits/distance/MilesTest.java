f8839fe004f2fd19f0b8ab385c4cdf3098174807
Segelzwerg <marcel.haas@hhu.de>
2020-02-19 22:40:46 +0100

    REFACTOR: use public tests

--- src/test/java/segelzwerg/sporttooolbox/iunits/distance/MilesTest.java
+++ src/test/java/segelzwerg/sporttooolbox/iunits/distance/MilesTest.java
@@ -9,12 +9,12 @@ import segelzwerg.sporttooolbox.iunits.speed.Speed;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
-class MilesTest {
+public class MilesTest {
 
     private final FloatComparator floatComparator = new FloatComparator(0.0001f);
 
     @Test
-    void constructorTest() {
+    public void constructorTest() {
         Miles miles = new Miles(1);
 
         Miles expectedMile = new Miles(1, 0);
@@ -24,7 +24,7 @@ class MilesTest {
 
 
     @Test
-    void overflowTest() {
+    public void overflowTest() {
         Miles miles = new Miles(0, 1761);
         Miles expectedMiles = new Miles(1, 1);
 
@@ -32,7 +32,7 @@ class MilesTest {
     }
 
     @Test
-    void addMilesTest() {
+    public void addMilesTest() {
         Miles miles = new Miles(0, 1759);
         Miles oneYard = new Miles(0, 1);
 
@@ -44,7 +44,7 @@ class MilesTest {
     }
 
     @Test
-    void sixtyMilesTestSpeed() {
+    public void sixtyMilesTestSpeed() {
         Miles miles = new Miles(60, 0);
         Time time = new Time(2, 0, 0);
         MilePerHour expectedSpeed = new MilePerHour(30f);
@@ -55,7 +55,7 @@ class MilesTest {
     }
 
     @Test
-    void speedWithYards() {
+    public void speedWithYards() {
         Miles yards = new Miles(0, 1250);
         Time time = new Time(2, 0, 0);
         Speed speed = yards.computeSpeed(time);
@@ -66,7 +66,7 @@ class MilesTest {
     }
 
     @Test
-    void negativeInpute() {
+    public void negativeInpute() {
         assertThatExceptionOfType(IllegalArgumentException.class).isThrownBy(() -> new Miles(-1, -1));
     }
 }
