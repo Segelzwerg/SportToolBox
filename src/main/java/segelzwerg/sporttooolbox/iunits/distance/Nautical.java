464ccb02efba8bfb64678123c294cd50667988b4
Segelzwerg <marcel.haas@hhu.de>
2020-02-19 22:49:15 +0100

    UPDATE: Nautical implements Distance

--- src/main/java/segelzwerg/sporttooolbox/iunits/distance/Nautical.java
+++ src/main/java/segelzwerg/sporttooolbox/iunits/distance/Nautical.java
@@ -1,6 +1,11 @@
 package segelzwerg.sporttooolbox.iunits.distance;
 
-class Nautical {
+import segelzwerg.sporttooolbox.iunits.Time;
+import segelzwerg.sporttooolbox.iunits.pace.Pace;
+import segelzwerg.sporttooolbox.iunits.speed.Speed;
+
+class Nautical implements Distance {
+    private final float NAUTICAL_TO_FATHOMS = 1012.68591f;
     private final int nautical;
     private final int fathoms;
 
@@ -11,5 +16,49 @@ class Nautical {
 
     Nautical(int nautical) {
         this(nautical, 0);
+    }
+
+    /**
+     * Add distance
+     *
+     * @param toAdd distance to add
+     * @return new distance
+     */
+    @Override
+    public Distance addDistance(Distance toAdd) {
+        return null;
+    }
+
+    /**
+     * Compute speed for specific time
+     *
+     * @param time Amount of time
+     * @return calculated speed
+     */
+    @Override
+    public Speed computeSpeed(Time time) {
+        return null;
+    }
+
+    /**
+     * Compute pace given a specific time
+     *
+     * @param time amount of time
+     * @return calculated pace
+     */
+    @Override
+    public Pace computePace(Time time) {
+        return null;
+    }
+
+    /**
+     * computes the time for a given speed
+     *
+     * @param speed {@link Speed}
+     * @return {@link Time}
+     */
+    @Override
+    public Time computeTime(Speed speed) {
+        return null;
     }
 }
