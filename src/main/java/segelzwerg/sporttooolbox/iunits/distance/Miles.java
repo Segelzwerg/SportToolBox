21f49fd5a13b3eab7a2d4f3110e7128a7d4febc5
Segelzwerg <marcel.haas@hhu.de>
2020-02-20 22:33:44 +0100

    UPDATE: handle negative input

--- src/main/java/segelzwerg/sporttooolbox/iunits/distance/Miles.java
+++ src/main/java/segelzwerg/sporttooolbox/iunits/distance/Miles.java
@@ -22,7 +22,10 @@ class Miles implements Distance {
         this.miles = (int) (miles + Math.floor(yards / MILES_TO_YARDS));
     }
 
-    public Miles(float miles) {
+    Miles(float miles) {
+        if (miles < 0) {
+            throw new IllegalArgumentException("Distances must not be negative. " + miles + "mi.");
+        }
         this.miles = (int) Math.abs(miles);
         yards = (int) ((miles - this.miles) * MILES_TO_YARDS);
     }
