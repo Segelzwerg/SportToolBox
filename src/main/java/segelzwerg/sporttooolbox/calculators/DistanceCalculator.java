bd47afb23bae57a960a831d799ed03cc10237144
Segelzwerg <marcel.haas@hhu.de>
2020-02-20 20:58:56 +0100

    ADD: distance calculation

---
+++ src/main/java/segelzwerg/sporttooolbox/calculators/DistanceCalculator.java
@@ -0,0 +1,19 @@
+package segelzwerg.sporttooolbox.calculators;
+
+import segelzwerg.sporttooolbox.iunits.Time;
+import segelzwerg.sporttooolbox.iunits.distance.Distance;
+import segelzwerg.sporttooolbox.iunits.speed.Speed;
+
+class DistanceCalculator {
+    private final Speed speed;
+    private final Time time;
+
+    DistanceCalculator(Speed speed, Time time) {
+        this.speed = speed;
+        this.time = time;
+    }
+
+    Distance computeDistance() {
+        return speed.computeDistance();
+    }
+}
