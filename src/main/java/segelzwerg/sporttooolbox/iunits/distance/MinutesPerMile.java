42b15ab36beb6c7cd2dffb3c92686caa5459c9b0
Segelzwerg <marcel.haas@hhu.de>
2020-02-20 13:03:43 +0100

    ADD: Minutes Per Mile

---
+++ src/main/java/segelzwerg/sporttooolbox/iunits/distance/MinutesPerMile.java
@@ -0,0 +1,43 @@
+package segelzwerg.sporttooolbox.iunits.distance;
+
+import segelzwerg.sporttooolbox.iunits.pace.Pace;
+import segelzwerg.sporttooolbox.iunits.speed.MeterPerSecond;
+import segelzwerg.sporttooolbox.iunits.speed.Speed;
+
+public class MinutesPerMile implements Pace {
+    private final float pace;
+
+    MinutesPerMile(float pace) {
+        this.pace = pace;
+    }
+
+    /**
+     * Converts to minutes per kilometers
+     *
+     * @return an instance of Pace
+     */
+    @Override
+    public Pace toMinutesPerKilometer() {
+        return null;
+    }
+
+    /**
+     * Converts to minutes per hundred-meters
+     *
+     * @return an instance of Pace
+     */
+    @Override
+    public Pace toMinutesPerHundredMeters() {
+        return null;
+    }
+
+    /**
+     * converts pace to speed
+     *
+     * @return {@link MeterPerSecond} as it is SI
+     */
+    @Override
+    public Speed getSpeed() {
+        return null;
+    }
+}
