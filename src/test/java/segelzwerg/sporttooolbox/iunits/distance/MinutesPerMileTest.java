6e896394f22fadb077aecb81001e2f94eb312a84
Segelzwerg <marcel.haas@hhu.de>
2020-02-20 14:30:42 +0100

    ADD: test for conversion to per KM

--- src/test/java/segelzwerg/sporttooolbox/iunits/distance/MinutesPerMileTest.java
+++ src/test/java/segelzwerg/sporttooolbox/iunits/distance/MinutesPerMileTest.java
@@ -1,5 +1,23 @@
 package segelzwerg.sporttooolbox.iunits.distance;
 
+import org.assertj.core.util.FloatComparator;
+import org.junit.jupiter.api.Test;
+import segelzwerg.sporttooolbox.iunits.pace.MinutesPerKilometer;
+import segelzwerg.sporttooolbox.iunits.pace.Pace;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class MinutesPerMileTest {
 
+    private final FloatComparator floatComparator = new FloatComparator(0.001f);
+
+    @Test
+    public void convertToMinutesPerKM() {
+        MinutesPerMile minutesPerMile = new MinutesPerMile(6f);
+        MinutesPerKilometer expectedPace = new MinutesPerKilometer(3.728227f);
+
+        Pace pace = minutesPerMile.toMinutesPerKilometer();
+
+        assertThat(pace).usingComparatorForFields(floatComparator, "pace").isEqualToComparingFieldByField(expectedPace);
+    }
 }
