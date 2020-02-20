1b5d612726e147f10143241166dc14b73d55b823
Segelzwerg <marcel.haas@hhu.de>
2020-02-20 14:07:37 +0100

    UPDATE: user override

--- src/main/java/segelzwerg/sporttooolbox/iunits/speed/MeterPerSecond.java
+++ src/main/java/segelzwerg/sporttooolbox/iunits/speed/MeterPerSecond.java
@@ -15,7 +15,7 @@ public class MeterPerSecond implements Speed {
     public static final String UNIT = "meter per second";
     private final float speed;
 
-    public MeterPerSecond(float speed) {
+    MeterPerSecond(float speed) {
         if (speed < 0) {
             throw new IllegalArgumentException("Speed must not be negative.");
         }
@@ -27,6 +27,7 @@ public class MeterPerSecond implements Speed {
      *
      * @return speed in kilometer per hour
      */
+    @Override
     public Speed toKilometerPerHour() {
         return new KilometerPerHour(speed * Speed.METER_PER_SECOND_TO_KILOMETER_PER_HOUR);
     }
@@ -36,6 +37,7 @@ public class MeterPerSecond implements Speed {
      *
      * @return speed in meter per second
      */
+    @Override
     public Speed toMeterPerSecond() {
         return this;
     }
@@ -45,6 +47,7 @@ public class MeterPerSecond implements Speed {
      *
      * @return speed in mile per hour
      */
+    @Override
     public Speed toMilePerHour() {
         return new MilePerHour(speed * Speed.METER_PER_SECOND_TO_KILOMETER_PER_HOUR / Speed.MILE_PER_HOUR_TO_KILOMETER_PER_HOUR);
     }
@@ -54,6 +57,7 @@ public class MeterPerSecond implements Speed {
      *
      * @return speed in knot
      */
+    @Override
     public Speed toKnot() {
         return new Knot(speed * Speed.METER_PER_SECOND_TO_KILOMETER_PER_HOUR / Speed.KNOT_TO_KILOMETER_PER_HOUR);
     }
