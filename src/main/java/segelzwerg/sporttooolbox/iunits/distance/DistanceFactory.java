309b2bf96bde6e69f46330d58472f3b530607698
Segelzwerg <marcel.haas@hhu.de>
2020-02-20 11:01:10 +0100

    ADD: constructor for kilometer

--- src/main/java/segelzwerg/sporttooolbox/iunits/distance/DistanceFactory.java
+++ src/main/java/segelzwerg/sporttooolbox/iunits/distance/DistanceFactory.java
@@ -1,7 +1,24 @@
 package segelzwerg.sporttooolbox.iunits.distance;
 
 public class DistanceFactory {
-    public static Distance createWithOtherThanSIUnits(int major, int minor, String majorUnit, String minorUnit) {
-        return null;
+    public static Distance createWithOtherThanSIUnits(int majorValue, int minorValue, String majorUnit, String minorUnit) {
+        Distance major;
+        switch (majorUnit) {
+            case "kilometer":
+                major = new Kilometer(majorValue);
+                break;
+            default:
+                throw new IllegalArgumentException("Unexpected unit: " + majorUnit);
+        }
+        Distance minor;
+        switch (minorUnit) {
+            case "meter":
+                minor = new Kilometer(0, minorValue);
+                break;
+            default:
+                throw new IllegalArgumentException("Unexpected unit: " + minorUnit);
+        }
+
+        return major.addDistance(minor);
     }
 }
