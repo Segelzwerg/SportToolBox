ac38a4e02f92b4de2262ded757a1fa5d09d8d422
Segelzwerg <marcel.haas@hhu.de>
2020-02-20 20:54:54 +0100

    ADD: test for calculating kilometer

--- src/test/java/segelzwerg/sporttooolbox/calculators/DistanceCalculatorTest.java
+++ src/test/java/segelzwerg/sporttooolbox/calculators/DistanceCalculatorTest.java
@@ -1,4 +1,24 @@
 package segelzwerg.sporttooolbox.calculators;
 
+import org.junit.jupiter.api.Test;
+import segelzwerg.sporttooolbox.iunits.Time;
+import segelzwerg.sporttooolbox.iunits.distance.Distance;
+import segelzwerg.sporttooolbox.iunits.distance.Kilometer;
+import segelzwerg.sporttooolbox.iunits.speed.KilometerPerHour;
+import segelzwerg.sporttooolbox.iunits.speed.Speed;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class DistanceCalculatorTest {
+    @Test
+    public void thirtyKilometer() {
+        Speed speed = new KilometerPerHour(30f);
+        Time oneHour = new Time(1, 0, 0);
+        DistanceCalculator distanceCalculator = new DistanceCalculator(speed, oneHour);
+        Distance expectedDistance = new Kilometer(30);
+
+        Distance kilometer = distanceCalculator.computeDistance();
+
+        assertThat(kilometer).isEqualToComparingFieldByField(expectedDistance);
+    }
 }
