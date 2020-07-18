e029cb9fc386ae8389b6896f627fb8493554cec1
Segelzwerg <marcel.haas@hhu.de>
2020-07-18 23:41:10 +0200

    ADD: constructor from pace

--- src/main/java/segelzwerg/sporttooolbox/calculators/DistanceCalculator.java
+++ src/main/java/segelzwerg/sporttooolbox/calculators/DistanceCalculator.java
@@ -2,6 +2,7 @@ package segelzwerg.sporttooolbox.calculators;
 
 import segelzwerg.sporttooolbox.iunits.Time;
 import segelzwerg.sporttooolbox.iunits.distance.Distance;
+import segelzwerg.sporttooolbox.iunits.pace.Pace;
 import segelzwerg.sporttooolbox.iunits.speed.Speed;
 
 public class DistanceCalculator {
@@ -11,6 +12,10 @@ public class DistanceCalculator {
     DistanceCalculator(Speed speed, Time time) {
         this.speed = speed;
         this.time = time;
+    }
+
+    DistanceCalculator(Pace pace, Time time) {
+        this(pace.getSpeed(), time);
     }
 
     public Distance computeDistance() {
